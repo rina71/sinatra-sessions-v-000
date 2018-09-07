472de29a945fb779cdf77fa9019f876054f33878
Yurina Shin <yurinas@gmail.com>
2018-09-07 23:21:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -54,6 +54,7 @@ class App < Sinatra::Base
   end
 
   get '/logout' do
+    session = {}
     #clear session hash here
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
